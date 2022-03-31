0789bfb10a72e5ce2ece4f2f286582d0fb5ad7ea
Nguyen Thanh Thien <true.cules4ever@gmail.com>
2022-03-31 17:41:17 +0700

    Update 31/3

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,6 @@
+class NewsController {
+    index(req, res){
+        res.render('news');
+    }
+}
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const { send } = require('process');
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //HTTP logger
 app.use(morgan('combined'));
 //Template engine
@@ -17,10 +22,19 @@ app.set('views', path.join(__dirname, '/resources/views'));
 
 app.get('/', (req, res) => {
   return res.render('home');
-})
-app.get('/news', (req, res) => {
-  return res.render('news');
-})
+});
+// app.get('/news', (req, res) => {
+//   return res.render('news');
+// });
+app.get('/search', (req, res) => {
+  //console.log(req.query.q);
+  return res.render('search');
+});
+app.post('/search', (req, res) => {
+  //console.log(req.query.q);
+  console.log(req.body)
+  res.send('');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
